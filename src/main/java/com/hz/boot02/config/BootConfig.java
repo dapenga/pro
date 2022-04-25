556b0be13334c7d2cb6012680526e14320015b1e
dapenga <1282568944@qq.com>
2022-04-25 11:08:33 +0800

    gdfg

---
+++ src/main/java/com/hz/boot02/config/BootConfig.java
@@ -0,0 +1,19 @@
+package com.hz.boot02.config;
+
+import com.hz.boot02.pojo.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//是标注该类为配置类， 读取启动类
+
+@Configuration
+public class BootConfig {
+    @Bean
+
+    public User newUser(){
+        User user = new User();
+        user.setUserName("张三");
+        return user;
+    }
+
+}
